ad87c3f0d5bf374a3562a03e988b51e3a523a767
Ana Clara Voltarelli Fenelon <145994699+d202007@users.noreply.github.com>
2025-03-24 12:16:04 +0000

    ana

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botãoDeacessibilidade = document.getElementById('botão-acessibilidade')
+    const opçõesDeacessibilade = document.getElementById('opções-acessibilidade')
+
+    botãoDeacessibilidade.addEventListener('click', function (){
+        botãoDeacessibilidade.classList.toggle('rotação-botão');
+        opçõesDeacessibilade.classList.toggle('apresenta-lista')
+    })
+
 const aumentafontebotão = document.getElementById('aumentar-fonte');
 const diminuirfontebotão = document.getElementById('diminuir-fonte');
 
